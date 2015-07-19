f7d58986d5e5162e4a4d82533f77f999dd2884f9
Noah Finer <nfiner0149@gmail.com>
2015-07-19 16:51:32 -0600

    Revamp contact and location pages and fix misc bugs

--- res/javascripts/header.js
+++ res/javascripts/header.js
@@ -1,5 +1,8 @@
 headerFixed = false;
 var dots = "../../";
+
+var compliments = ["Your smile is contagious.", "You have the best laugh.", "You're an awesome person.", "You're more helpful than you realize.", "Is that your picture next to charming in the dictionary?", "On a scale from 1 to 10, you're an 11.", "Your inside is even more beautiful than your outside.", "You're like sunshine on a rainy day.", "You bring out the best in other people.", "Being around you makes everything better.", "Jokes are funnier when you tell them.", "You're better than a triple-scoop ice cream cone. With sprinkles.", "You're inspiring.", "If you were a box of crayons, you'd be the big industrial name-brand one with a built-in sharpener.", "Our community is better because you're in it.", "You always know how to find that silver lining.", "Everyone gets knocked down sometimes; only people like you get back up again and keep going.", "You're more fun than bubble wrap.", "You're like a breath of fresh air.", "You're gorgeous... and that's the least interesting thing about you, too.", "When you make up your mind, nothing stands in your way.", "In high school I bet you were voted most likely to continue being awesome.", "You're even better than a unicorn, because you're real."];
+
 $(document).ready(function() {
   $('.intro-image-header').css("background-position-y", "-50px");
   var number = Math.floor(Math.random()*37);
@@ -95,6 +98,7 @@ $(document).ready(function() {
     $('#equipment-dropdown').addClass('shown');
   });
   setTimeout(function() {$('.intro-image-header').css("background-image", "url('"+dots+"res/images/inspirational/i"+number+".jpg')");}, 10);
+  $("#compliment").html(compliments[Math.floor(Math.random()*compliments.length)]);
 })
 
 
