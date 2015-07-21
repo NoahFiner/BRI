52633a07e0117931c828edad125887faaa1f8571
Noah Finer <nfiner0149@gmail.com>
2015-07-21 11:31:35 -0600

    Fix a few IE bugs and general things

--- res/javascripts/header.js
+++ res/javascripts/header.js
@@ -37,69 +37,94 @@ $(document).ready(function() {
   setTimeout(function() {
     $('#header-top-outer').addClass("background-img");
   }, 250);
+  var timeoutthing;
+  var timeywimey = function(wat) {
+    clearTimeout(timeoutthing);
+    timeoutthing =setTimeout(function() {$('#'+wat+'-dropdown').removeClass("shown")}, 5000);
+  }
 
   $('#about-link, #about-dropdown').hover(function() {
     $('#about-dropdown').addClass('shown');
+    timeywimey('about');
   }, function() {
     $('#about-dropdown').removeClass('shown');
   });
 
   $('#about-link').click(function()  {
     $('#about-dropdown').addClass('shown');
+    timeywimey('about');
   });
 
   $('#links-link, #links-dropdown').hover(function() {
     $('#links-dropdown').addClass('shown');
+    timeywimey('links');
   }, function() {
     $('#links-dropdown').removeClass('shown');
   });
 
   $('#links-link').click(function()  {
     $('#links-dropdown').addClass('shown');
+    timeywimey('links');
   });
 
   $('#us-link, #us-dropdown').hover(function() {
     $('#us-dropdown').addClass('shown');
+    timeywimey('us');
   }, function() {
     $('#us-dropdown').removeClass('shown');
   });
 
   $('#us-link').click(function()  {
     $('#us-dropdown').addClass('shown');
+    timeywimey('us');
   });
 
   $('#services-link, #services-dropdown').hover(function() {
     $('#services-dropdown').addClass('shown');
+    timeywimey('services');
   }, function() {
     $('#services-dropdown').removeClass('shown');
   });
 
   $('#services-link').click(function()  {
+    timeywimey('services');
     $('#services-dropdown').addClass('shown');
   });
 
   $('#contact-link, #contact-dropdown').hover(function() {
     $('#contact-dropdown').addClass('shown');
+    timeywimey('contact');
   }, function() {
     $('#contact-dropdown').removeClass('shown');
   });
 
   $('#contact-link').click(function()  {
     $('#contact-dropdown').addClass('shown');
+    timeywimey('contact');
   });
 
   $('#equipment-link, #equipment-dropdown').hover(function() {
     $('#equipment-dropdown').addClass('shown');
+    timeywimey('equipment');
   }, function() {
     $('#equipment-dropdown').removeClass('shown');
   });
 
   $('#equipment-link').click(function()  {
+    timeywimey('equipment');
     $('#equipment-dropdown').addClass('shown');
   });
   setTimeout(function() {$('.intro-image-header').css("background-image", "url('"+dots+"res/images/inspirational/i"+number+".jpg')");}, 10);
   $("#compliment").html(compliments[Math.floor(Math.random()*compliments.length)]);
+  if($('html').is(('.ie6, .ie7, .ie8'))) {
+    ieWarning();
+  }
 })
+
+var ieWarning = function() {
+  alert("It appears you're using an outdated version of Internet Explorer to view this site. Unfortunately, Internet Explorer does not support a lot of this website's features. Try viewing this site on Google Chrome for a better experience.");
+}
+
 
 
 $(function() { // Smooth scrolling with the pages with the hashtag stuff. Idk why it works.
